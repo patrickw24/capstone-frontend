b33e863d84d5d108ee9a7f3a2508efb258987efe
patrickw24 <patrick.warner24@gmail.com>
2025-03-07 21:19:51 -0600

    created component folders

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Navbar } from './Components/Navbar'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 
-import './App.css'
+
 
 function App() {
   
@@ -11,8 +11,9 @@ function App() {
   return (
    <>
     <BrowserRouter> 
+    <Navbar/>
     <Routes>
-      <Route path='/' element={<Navbar/>}/>
+      <Route />
     </Routes>
     </BrowserRouter>
    </>
